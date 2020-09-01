a834ad9b24f808582ddc2e40004e28c13caf14b4
fonysaputra <fonysaputra95@gmail.com>
2020-09-01 08:22:24 +0700

    update 2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
+
 export default function Home() {
   const router = useRouter()
-  router.push('/login')
+  useEffect(() => {
+    router.push('/login')
+  }, [])
   return (
     <div className="container">
       <button className="btn-primary">loading</button>
