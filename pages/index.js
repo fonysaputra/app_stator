7fc9febe68a264bb9ae1ef8fd256ceb04afbad3a
fonysaputra <fonysaputra95@gmail.com>
2020-09-01 08:20:01 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { useRouter } from 'next/router'
 export default function Home() {
+  const router = useRouter()
+  router.push('/login')
   return (
     <div className="container">
-      <button className="btn-primary">dsxcd</button>
+      <button className="btn-primary">loading</button>
     </div>
   )
 }
